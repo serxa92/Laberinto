98c74e244d7901dd8123383490e6fb33635a447d
Sergio Agulla Pouseu <serxa92@hotmail.com>
2024-12-29 12:57:53 +0100

    final?

--- Laberinto.java
+++ Laberinto.java
@@ -1,111 +1,105 @@
+import java.util.Scanner;
+import java.util.Random;
 
-            // Procesar la opción ingresada
-           import java.util.Scanner;
+public class Laberinto {
 
-            public class Laberinto {
+    public static boolean IS_GAME_OVER = false;
+    public static int energyPoints = 100;
+    static Random random = new Random();
+    public static short roomPosition = 0;
+    public static int[][] rooms = {
+            {2, 3},
+            {1, 4},
+            {},
+            {1, 2}
+    };
 
-                public static boolean IS_GAME_OVER = false;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("****Bienvenido al juego del laberinto ****\n");
 
-                public static int gamePoints = 100;
+        while (!IS_GAME_OVER) {
+            System.out.println("Estás en la sala " + (roomPosition + 1));
+            System.out.println("Que quieres hacer?\n" +
+                    "1. Moverte de sala\n" +
+                    "2. Inspeccionar la sala\n" +
+                    "3. Salir del juego.");
 
-                public static short roomPosition = 0;
+            short option = getValidOption(sc);
 
-                public static int[][] rooms = new int[4][2];
-
-                public static short[] randomEvents= new short [3];
-
-                public static void main(String[] args) {
-
-                    // Para acceder a la posicion n de un array, abrimos [] y ponemos el índice n
-                    rooms[0] = new int[]{2, 3};
-                    rooms[1] = new int[]{1, 4};
-                    rooms[2] = new int[]{};
-                    rooms[3] = new int[]{1, 2};
-
-                    Scanner sc = new Scanner(System.in);
-
-                    while (!IS_GAME_OVER) {
-                        System.out.println("Estás en la sala " + (roomPosition + 1));
-                        System.out.println("Que quieres hacer?\n" +
-                                "Opción 1: Moverte de sala\n" +
-                                "Opción 2: Inspeccionar la sala\n" +
-                                "Opción 3: Salir del juego.");
-
-                        short option = 0; // Inicializamos la opción
-                        boolean opcionvalida = false;
-
-                        // Validar entrada del usuario
-                        do {
-                            if (sc.hasNextShort()) {
-                                option = sc.nextShort();
-                                if (option >= 1 && option <= 3) {
-                                    opcionvalida = true;
-                                } else {
-                                    System.out.println("Opción fuera de rango. Debe ser 1, 2 o 3.");
-                                }
-                            } else {
-                                System.out.println("Entrada no válida. Por favor, introduce un número.");
-                                sc.next();
-                            }
-                        } while (!opcionvalida);
-
-                        // Procesamos la opción ingresada
-
-                        switch (option){
-
-                            case 1 ->{
-                                System.out.println("¿A que sala quieres moverte?");
-
-                                short room = sc.nextShort();
-
-                                // Dentro del array rooms, accedo a la posicion actual y evalúo las posibilidades de conexión
-                                // en los indices [0] y [1]
-
-                                if (rooms[roomPosition][0] ==room || rooms[roomPosition][1]==room){
-
-                                    if (room==3){
-                                        System.out.println("¡Has llegado al final!");
-                                        IS_GAME_OVER = true;
-                                    }else {
-                                        System.out.println("Puedes acceder a esta sala");
-                                    }
-
-                                    roomPosition = (short) (room - 1);
-
-                                }else{
-                                    System.out.println("No puedes acceder a esta sala");
-                                }
-                                break;
-
-
-                            }
-
-                            case 2 ->{
-
-
-
-
-                                break;
-                            }
-
-                            case 3 ->{
-
-                                System.out.println("Has salido del juego");
-                                IS_GAME_OVER = true;
-
-
-                                break;
-                            }}
-                    }
-
-                    sc.close();
-                }
+            if (energyPoints <= 0) {
+                System.out.println("¡Te has quedado sin energía! Has perdido el juego.");
+                IS_GAME_OVER = true;
+                continue;
             }
 
+            switch (option) {
+                case 1 -> moveRoom(sc);
+                case 2 -> inspectRoom();
+                case 3 -> {
+                    System.out.println("Has decidido salir del juego. ¡Hasta la próxima!");
+                    IS_GAME_OVER = true;
+                }
+            }
+        }
+        sc.close();
+    }
 
+    private static short getValidOption(Scanner sc) {
+        short option = 0;
+        boolean validOption = false;
+        while (!validOption) {
+            if (sc.hasNextShort()) {
+                option = sc.nextShort();
+                if (option >= 1 && option <= 3) {
+                    validOption = true;
+                } else {
+                    System.out.println("Opción fuera de rango. Debe ser 1, 2 o 3.");
+                }
+            } else {
+                System.out.println("Entrada no válida. Por favor, introduce un número.");
+                sc.next();
+            }
+        }
+        return option;
+    }
 
+    private static void moveRoom(Scanner sc) {
+        System.out.println("¿A que sala quieres moverte?");
+        short room = sc.nextShort();
+        if (rooms[roomPosition][0] == room || rooms[roomPosition][1] == room) {
+            if (room == 3) {
+                System.out.println("¡Has llegado al final!");
+                IS_GAME_OVER = true;
+            }
+            roomPosition = (short) (room - 1);
+        } else {
+            System.out.println("No puedes acceder a la sala " + room);
+        }
+    }
 
+    private static void inspectRoom() {
+        System.out.println("Has decidido inspeccionar la sala, veamos...");
+        int points = getInspectionPoints();
+        energyPoints += points;
+    }
 
-
-
-
+    public static int getInspectionPoints() {
+        int points = 0;
+        int randomEvent = random.nextInt(3);
+        switch (randomEvent) {
+            case 0 -> {
+                System.out.println("¡Has encontrado un tesoro! ¡Has aumentado tu energía en 10 puntos!\n" +
+                        "Tu energía actual es: " + (energyPoints + 10));
+                points = 10;
+            }
+            case 1 -> {
+                System.out.println("¡Has caído en una trampa, has reducido tu energía en 15 puntos!\n" +
+                        "Tu energía actual es: " + (energyPoints - 15));
+                points = -15;
+            }
+            case 2 -> System.out.println("No has encontrado nada...");
+        }
+        return points;
+    }
+}
